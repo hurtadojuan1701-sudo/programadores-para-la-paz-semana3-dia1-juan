b9d3936d6c8beab2bde840211e72451490499a63
hurtadojuan1701-sudo <hurtadojuan1701@gmail.com>
2026-05-08 14:51:21 -0500

    aprendiendo algoritmos javascrip semana3

---
+++ javascript/ejemplo.js
@@ -0,0 +1,14 @@
+console.log("HOLA A TODOS ESTAMOS APRENDIENDO");
+console.log("HOY ESTAMOS TRABAJANDO JAVASCRIPT");
+
+var guardado = "juan david";
+
+console.log(guardado);
+
+let paises = ["colombia", "peru", "argentina", "chile", "brasil"];
+
+const no_cambiar = "no cambiar";
+
+console.log(paises);
+
+console.log(paises);
---
+++ javascript/ensayo.js
@@ -0,0 +1,13 @@
+const propuesta = "Aprender a usar javascrip"
+let sabelobasicodeinternet = true
+let tieneundispositivo = true
+let tieneinternet = true
+
+console.log("revision de requisitos para aprender a usar javascript");
+console.log("propuesta:", propuesta);
+
+if (sabelobasicodeinternet === true && tieneundispositivo === true && tieneinternet === true) {
+    console.log("Cumples con los requisitos para aprender a usar javascript");} 
+    else {
+    console.log("No cumples con los requisitos para aprender a usar javascript");
+}
